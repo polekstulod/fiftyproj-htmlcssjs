0c566429f7ed2ceb76376e71dd1328a5269c691c
paultulod <paultulod@pm.me>
2022-06-18 23:00:00 +0800

    Fix format

--- 3d-boxes-background/script.js
+++ 3d-boxes-background/script.js
@@ -4,14 +4,14 @@ const btn = document.getElementById('btn')
 btn.addEventListener('click', () => boxContainer.classList.toggle('big'))
 
 function createBoxes() {
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            const box = document.createElement('div')
-            box.classList.add('box')
-            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
-            boxContainer.appendChild(box)
-        }
-    }
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			const box = document.createElement('div')
+			box.classList.add('box')
+			box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+			boxContainer.appendChild(box)
+		}
+	}
 }
 
 createBoxes()

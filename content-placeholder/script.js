1cbd8038a5247f073c4e972cbdbe507784fc839c
paultulod <paultulod@pm.me>
2022-07-08 22:08:33 +0800

    Fix format

--- content-placeholder/script.js
+++ content-placeholder/script.js
@@ -11,16 +11,16 @@ const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 setTimeout(getData, 2500)
 
 function getData() {
-    header.innerHTML =
-        '<img src="https://pbs.twimg.com/media/FRzVITxXsAE6dDr?format=jpg&name=4096x4096" alt="EU"/>'
-    title.innerHTML = 'Lorem ipsum dolor sit amet.'
-    excerpt.innerHTML =
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
-    profile_img.innerHTML =
-        '<img src="https://pbs.twimg.com/media/FQyqi7WVgAAkVR4?format=jpg&name=large" alt="women"/>'
-    name.innerHTML = 'Park Jiwon'
-    date.innerHTML = 'September 21, 2020'
+	header.innerHTML =
+		'<img src="https://pbs.twimg.com/media/FRzVITxXsAE6dDr?format=jpg&name=4096x4096" alt="EU"/>'
+	title.innerHTML = 'Lorem ipsum dolor sit amet.'
+	excerpt.innerHTML =
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+	profile_img.innerHTML =
+		'<img src="https://pbs.twimg.com/media/FQyqi7WVgAAkVR4?format=jpg&name=large" alt="women"/>'
+	name.innerHTML = 'Park Jiwon'
+	date.innerHTML = 'September 21, 2020'
 
-    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
-    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
+	animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+	animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
 }

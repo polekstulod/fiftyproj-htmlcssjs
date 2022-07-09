b837703a8f7304e75999325e694d978802331933
paultulod <paultulod@pm.me>
2022-07-09 22:19:37 +0800

    Fix format

--- drag-n-drop/script.js
+++ drag-n-drop/script.js
@@ -5,30 +5,30 @@ fill.addEventListener('dragstart', dragStart)
 fill.addEventListener('dragend', dragEnd)
 
 for (const empty of empties) {
-    empty.addEventListener('dragover', dragOver)
-    empty.addEventListener('dragenter', dragEnter)
-    empty.addEventListener('dragleave', dragLeave)
-    empty.addEventListener('drop', dragDrop)
+	empty.addEventListener('dragover', dragOver)
+	empty.addEventListener('dragenter', dragEnter)
+	empty.addEventListener('dragleave', dragLeave)
+	empty.addEventListener('drop', dragDrop)
 }
 
 function dragStart() {
-    this.className += ' hold'
-    setTimeout(() => (this.className = 'invisible'), 0)
+	this.className += ' hold'
+	setTimeout(() => (this.className = 'invisible'), 0)
 }
 function dragEnd() {
-    this.className = 'fill'
+	this.className = 'fill'
 }
 function dragOver(e) {
-    e.preventDefault()
+	e.preventDefault()
 }
 function dragEnter(e) {
-    e.preventDefault()
-    this.className += ' hovered'
+	e.preventDefault()
+	this.className += ' hovered'
 }
 function dragLeave(e) {
-    this.className = 'empty'
+	this.className = 'empty'
 }
 function dragDrop() {
-    this.className = 'empty'
-    this.append(fill)
+	this.className = 'empty'
+	this.append(fill)
 }

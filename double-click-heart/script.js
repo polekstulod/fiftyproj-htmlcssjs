f358dc93500180c93a063e206687c0dfb9292d55
paultulod <paultulod@pm.me>
2022-07-09 22:19:08 +0800

    Fix format

--- double-click-heart/script.js
+++ double-click-heart/script.js
@@ -5,40 +5,40 @@ let clickTime = 0
 let timesclicked = 0
 
 loveme.addEventListener('click', e => {
-    if (clickTime === 0) {
-        clickTime = new Date().getTime()
-    } else {
-        if (new Date().getTime() - clickTime < 800) {
-            createHeart(e)
-            clickTime = 0
-        } else {
-            clickTime = new Date().getTime()
-        }
-    }
+	if (clickTime === 0) {
+		clickTime = new Date().getTime()
+	} else {
+		if (new Date().getTime() - clickTime < 800) {
+			createHeart(e)
+			clickTime = 0
+		} else {
+			clickTime = new Date().getTime()
+		}
+	}
 })
 
 const createHeart = e => {
-    const heart = document.createElement('i')
-    heart.classList.add('fas')
-    heart.classList.add('fa-heart')
+	const heart = document.createElement('i')
+	heart.classList.add('fas')
+	heart.classList.add('fa-heart')
 
-    const x = e.clientX
-    const y = e.clientY
+	const x = e.clientX
+	const y = e.clientY
 
-    const leftOffset = e.target.offsetLeft
-    const topOffset = e.target.offsetTop
+	const leftOffset = e.target.offsetLeft
+	const topOffset = e.target.offsetTop
 
-    const xInside = x - leftOffset
-    const yInside = y - topOffset
+	const xInside = x - leftOffset
+	const yInside = y - topOffset
 
-    heart.style.top = `${yInside}px`
-    heart.style.left = `${xInside}px`
+	heart.style.top = `${yInside}px`
+	heart.style.left = `${xInside}px`
 
-    loveme.appendChild(heart)
+	loveme.appendChild(heart)
 
-    times.innerHTML = ++timesclicked
+	times.innerHTML = ++timesclicked
 
-    setTimeout(() => {
-        heart.remove()
-    }, 1000)
+	setTimeout(() => {
+		heart.remove()
+	}, 1000)
 }

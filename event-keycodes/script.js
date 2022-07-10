7bd519863c5551e4d647938de03fb7e8792df216
paultulod <paultulod@pm.me>
2022-07-10 23:52:04 +0800

    Fix format

--- event-keycodes/script.js
+++ event-keycodes/script.js
@@ -1,7 +1,7 @@
 const insert = document.getElementById('insert')
 
 window.addEventListener('keydown', e => {
-    insert.innerHTML = `
+	insert.innerHTML = `
     <div class="key">
     ${e.key === ' ' ? 'Space' : e.key}
     <small>event.key</small>

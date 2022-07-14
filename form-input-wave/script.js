e6e09ca0bb35e90f78bad8df6b7d318be4d47acb
paultulod <paultulod@pm.me>
2022-07-14 23:47:49 +0800

    Fix format

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -1,11 +1,11 @@
 const labels = document.querySelectorAll('.form-control label')
 
-labels.forEach((label) => {
-    label.innerHTML = label.innerText
-        .split('')
-        .map(
-            (letter, idx) =>
-                `<span style ='transition-delay:${idx * 50}ms'>${letter}</span>`
-        )
-        .join('')
+labels.forEach(label => {
+	label.innerHTML = label.innerText
+		.split('')
+		.map(
+			(letter, idx) =>
+				`<span style ='transition-delay:${idx * 50}ms'>${letter}</span>`
+		)
+		.join('')
 })

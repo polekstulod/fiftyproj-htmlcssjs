94fad246e3644a44e7b6a428f7119b998e27d53e
paultulod <paultulod@pm.me>
2022-07-16 22:12:48 +0800

    Fix format

--- image-carousel/script.js
+++ image-carousel/script.js
@@ -9,34 +9,34 @@ let idx = 0
 let interval = setInterval(run, 2000)
 
 function run() {
-    idx++
+	idx++
 
-    changeImage()
+	changeImage()
 }
 
 function changeImage() {
-    if (idx > img.length - 1) {
-        idx = 0
-    } else if (idx < 0) {
-        idx = img.length - 1
-    }
+	if (idx > img.length - 1) {
+		idx = 0
+	} else if (idx < 0) {
+		idx = img.length - 1
+	}
 
-    imgs.style.transform = `translateX(${-idx * 500}px)`
+	imgs.style.transform = `translateX(${-idx * 500}px)`
 }
 
 function resetInterval() {
-    clearInterval(interval)
-    interval = setInterval(run, 2000)
+	clearInterval(interval)
+	interval = setInterval(run, 2000)
 }
 
 rightBtn.addEventListener('click', () => {
-    idx++
-    changeImage()
-    resetInterval()
+	idx++
+	changeImage()
+	resetInterval()
 })
 
 leftBtn.addEventListener('click', () => {
-    idx--
-    changeImage()
-    resetInterval()
+	idx--
+	changeImage()
+	resetInterval()
 })

4dd622762415b15a63dcc4bc58f797383014655c
paultulod <paultulod@pm.me>
2022-07-18 23:53:14 +0800

    Fix format

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,8 +1,8 @@
 const API_URL =
-    'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+	'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API =
-    'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+	'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
@@ -12,21 +12,21 @@ const search = document.getElementById('search')
 getMovies(API_URL)
 
 async function getMovies(url) {
-    const res = await fetch(url)
-    const data = await res.json()
-    showMovies(data.results)
+	const res = await fetch(url)
+	const data = await res.json()
+	showMovies(data.results)
 }
 
 function showMovies(movies) {
-    main.innerHTML = ''
+	main.innerHTML = ''
 
-    movies.forEach(movie => {
-        const { title, poster_path, vote_average, overview } = movie
+	movies.forEach(movie => {
+		const { title, poster_path, vote_average, overview } = movie
 
-        const movieEl = document.createElement('div')
-        movieEl.classList.add('movie')
+		const movieEl = document.createElement('div')
+		movieEl.classList.add('movie')
 
-        movieEl.innerHTML = ` 
+		movieEl.innerHTML = ` 
                 <img src="${IMG_PATH + poster_path}" alt="${title}" />
                 <div class="movie-info">
                     <h3>${title}</h3>
@@ -39,26 +39,26 @@ function showMovies(movies) {
                     ${overview}
                 </div>
         `
-        main.appendChild(movieEl)
-    })
+		main.appendChild(movieEl)
+	})
 }
 
 function getClassByRate(vote) {
-    if (vote >= 8) return 'green'
-    else if (vote >= 5) return 'orange'
-    else return 'red'
+	if (vote >= 8) return 'green'
+	else if (vote >= 5) return 'orange'
+	else return 'red'
 }
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
+	e.preventDefault()
 
-    const searchTerm = search.value
+	const searchTerm = search.value
 
-    if (searchTerm && searchTerm !== '') {
-        getMovies(SEARCH_API + searchTerm)
+	if (searchTerm && searchTerm !== '') {
+		getMovies(SEARCH_API + searchTerm)
 
-        search.value = ''
-    } else {
-        window.location.reload()
-    }
+		search.value = ''
+	} else {
+		window.location.reload()
+	}
 })
